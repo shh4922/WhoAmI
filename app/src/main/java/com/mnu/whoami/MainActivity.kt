169b1b18988b92e385df886c14a35dc1cdaffafa
Sin Hyeon Ho <gusgh4922@gmail.com>
2022-11-15 15:35:59 +0900

    intro 및 ui

--- app/src/main/java/com/mnu/whoami/MainActivity.kt
+++ app/src/main/java/com/mnu/whoami/MainActivity.kt
@@ -1,52 +1,46 @@
 package com.mnu.whoami
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.icu.number.NumberFormatter.with
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.gun0912.tedpermission.PermissionListener
-import com.gun0912.tedpermission.normal.TedPermission
 import com.mnu.whoami.databinding.ActivityMainBinding
-import java.security.Permission
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    val REQUEST_IMG_CAPTURE  =1
-    lateinit var curPhotoPath : String
+    val CAMERA_PERMISSION = arrayOf(android.Manifest.permission.CAMERA)
+    val STORAGE_PERMISSION = arrayOf(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    val FLAG_PERM_CAMERA = 98
+    val FLAG_PERM_STORAGE = 99
+
+    val FLAG_REQ_CAMERA = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnCamera.setOnClickListener(this)
         binding.btnAlbum.setOnClickListener(this)
 
-        setPermission()
+
 
     }
-    private fun showToat(msg:String){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
-    }
-    private fun setPermission() {
-        val permission = object  : PermissionListener{
-            override fun onPermissionGranted() {//권한허용 완료
-                showToat("허용완료")
-            }
 
-            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                showToat("권한 거부")
-            }
-
-        }
-
+    private fun showToat(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
     /***
@@ -54,18 +48,76 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      */
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.btn_camera->{
-                //카메라 엑피비티 수행
+        when (v?.id) {
+            R.id.btn_camera -> {
 
+                if (isPermitted(CAMERA_PERMISSION)) {
+                    openCamera()
+                } else {
+                    ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, FLAG_PERM_CAMERA)
+                }
             }
-            R.id.btn_album->{
+            R.id.btn_album -> {
                 //로컬엘범에 접근
             }
         }
     }
 
+    fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, FLAG_REQ_CAMERA)
 
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==Activity.RESULT_OK){
+            when(requestCode){
+                FLAG_REQ_CAMERA->{
+                    val bitmap = data?.extras?.get("data") as Bitmap
+                    binding.faceImg.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+    }
+
+
+
+    //
+    fun isPermitted(permissions: Array<String>): Boolean {
+
+        val result = ContextCompat.checkSelfPermission(this, permissions.toString())
+        if (result != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            FLAG_PERM_CAMERA->{
+                var checked = true
+                for(grant in grantResults){
+                    if (grant!= PackageManager.PERMISSION_GRANTED){
+                        checked =false
+                        break
+                    }
+                }
+                if(checked){
+                    openCamera()
+                }
+            }
+
+
+        }
+    }
 }
 
 
